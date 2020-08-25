54cc2a13f80a650918f5649908b6439ae6bd5a89
Thomas Gouin <thomas.gouin@gmail.com>
2020-08-25 10:54:24 +0200

    ajout du modèle pour les utilisateurs

---
+++ front/src/models/user.js
@@ -0,0 +1,11 @@
+//Voici notre modèle User qui prend les mêmes caractéristiques que dans notre BDD. 
+
+export default class User {
+    constructor(firstname, lastname, email, password) {
+      this.firstrname = firstname;
+      this.lastrname = lastname;
+      this.email = email;
+      this.password = password;
+    }
+}
+  
