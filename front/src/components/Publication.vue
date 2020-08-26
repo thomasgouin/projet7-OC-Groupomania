2e29d07a3b29616c5f7d9cdd2e00fb9ed002f33d
Thomas Gouin <thomas.gouin@gmail.com>
2020-08-26 12:44:12 +0200

    template d'un article sur vuejs

---
+++ front/src/components/Publication.vue
@@ -0,0 +1,73 @@
+<template>
+    <div class="publication">
+        <div class="publication__header">
+            <img src="@/assets/profile-pic.png" alt="image de profil par défaut" class="publication__header__img">
+            <p class="publication__header__auteur">
+                Thomas Gouin
+            </p>
+        </div>
+        <div class="publication__main">
+            <p class="publication__main__texte">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Ad minus quisquam tenetur laborum qui nostrum ipsum explicabo omnis minima
+            </p>
+            <img src="@/assets/Logo-rouge.png" alt="Image ou git publiée par un utilisateur" class="publication__main__img">
+        </div>
+        <div class="publication__footer">
+            <p class="publication__footer__date">
+                25/08/2020
+            </p>
+            <div class="publication__footer__social">
+                <img src="@/assets/comment-solid.png" alt="icone pour les commentaires" class="icones-social">
+            </div>
+
+        </div>
+    </div>
+
+</template>
+<script>
+export default {
+    name:"Publication",
+    components: {},
+    data(){
+        return {}
+    }
+};
+</script>
+<style lang="scss" >
+
+.publication{
+    background-color: #ffff;
+    &__header{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 10px;
+        &__img{
+            max-width: 50px;
+        }
+        &__auteur{
+            font-size: 20px;
+        }
+    }
+    &__main{
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        &__img{
+            max-width: 270px;
+            border: solid 1px #042255;
+        }
+    }
+    &__footer{
+        margin-top: 10px;
+        display: flex;
+        justify-content: space-between;
+    }
+}
+.icones-social{
+    max-width: 30px;
+}
+    
+</style>
